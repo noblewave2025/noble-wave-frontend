6a7b07d7b6a983d6850759364092c4f41176a0ec
iamfromhe1l <sgadgsfad@gmail.com>
2025-08-03 21:40:12 +0300

    header buttons init

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,3 +1,4 @@
+import { HeaderButtons } from './HeaderButton';
 import { HeaderLinks } from './HeaderLinks';
 
 export const Header = () => {
@@ -5,7 +6,7 @@ export const Header = () => {
     <header className="bg-secondary/30 border-border/50 fixed right-2 left-2 my-2 flex items-center justify-between rounded-xl border px-6 py-2 backdrop-blur-[1px]">
       Logo
       <HeaderLinks />
-      Buttons
+      <HeaderButtons />
     </header>
   );
 };
