0caa1bf78e71ea5354cf127372a09528367e203a
iamfromhe1l <sgadgsfad@gmail.com>
2025-08-12 13:28:37 -0700

    Squashed commit of the following:
    commit 0c900e900dee2d1f2dfc945f66ef7db264dcf885
    Author: iamfromhe1l <sgadgsfad@gmail.com>
    Date:   Tue Aug 12 13:27:36 2025 -0700
    
        header base adaptive
    
    commit eeff36f4688ffa2ce32c10487b37abcbacee39ac
    Author: iamfromhe1l <sgadgsfad@gmail.com>
    Date:   Sun Aug 3 21:44:02 2025 +0300
    
        header logo init
    
    commit 6a7b07d7b6a983d6850759364092c4f41176a0ec
    Author: iamfromhe1l <sgadgsfad@gmail.com>
    Date:   Sun Aug 3 21:40:12 2025 +0300
    
        header buttons init
    
    commit 3f129607a680ef1b50e8a89dae88b63588cccd23
    Author: iamfromhe1l <sgadgsfad@gmail.com>
    Date:   Sun Aug 3 21:33:59 2025 +0300
    
        header links
    
    commit eaa38ec73b8c29500d574195fa6d41bfae197a9a
    Author: iamfromhe1l <sgadgsfad@gmail.com>
    Date:   Thu Jul 31 23:22:16 2025 +0300
    
        header base template with link
    
    commit da7fd26ff800fab7ab1a7725d731e93ec27e0637
    Author: iamfromhe1l <sgadgsfad@gmail.com>
    Date:   Thu Jul 31 23:01:19 2025 +0300
    
        header init

--- components/header/HeaderLogo.tsx
+++ components/header/HeaderLogo.tsx
@@ -1,5 +1,13 @@
 import Image from 'next/image';
 
 export const HeaderLogo = () => {
-  return <Image src="/icon.ico" width="30" height="30" alt="logo" />;
+  return (
+    <Image
+      className="absolute left-1/2 -translate-x-1/2 sm:relative sm:left-0 sm:translate-x-0"
+      src="/icon.ico"
+      width="30"
+      height="30"
+      alt="logo"
+    />
+  );
 };
