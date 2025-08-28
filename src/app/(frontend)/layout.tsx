4eecb4dcfd5c263fcce1c8e17fd6fff4c68d4df0
iamfromhe1l <sgadgsfad@gmail.com>
2025-08-28 11:44:34 -0700

    Squashed commit of the following:
    commit 63c2516db6104307ba3f63c73ac431b3cf40b18e
    Author: iamfromhe1l <sgadgsfad@gmail.com>
    Date:   Thu Aug 28 11:43:39 2025 -0700
    
        payload init

---
+++ src/app/(frontend)/layout.tsx
@@ -0,0 +1,35 @@
+import { Metadata } from 'next';
+import React from 'react';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  description: 'A blank template using Payload in a Next.js app.',
+  title: 'Payload Blank Template',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} relative antialiased`}
+      >
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
