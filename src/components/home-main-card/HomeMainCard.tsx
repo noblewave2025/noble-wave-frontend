167a81567639eef1cabd587c9f5d6c83a7737d13
iamfromhe1l <sgadgsfad@gmail.com>
2025-09-05 22:48:58 +0300

    Squashed commit of the following:
    commit 5cb74a3c49c6d71243c21e110c56472453f8cf86
    Author: iamfromhe1l <sgadgsfad@gmail.com>
    Date:   Sun Aug 31 20:28:05 2025 +0300
    
        remove file
    
    commit 27b9f8ba63d4d50361075517a4b08489ec88b942
    Author: iamfromhe1l <sgadgsfad@gmail.com>
    Date:   Sun Aug 31 20:16:57 2025 +0300
    
        Squashed commit of the following:
    
        commit 24674e2ca4cec2c614dd199ceef39a5e8aae636c
        Author: iamfromhe1l <sgadgsfad@gmail.com>
        Date:   Sun Aug 31 20:16:20 2025 +0300
    
            remove file
    
        commit c3b276e19a60771f7e56460b444b54a8b4ac26de
        Author: iamfromhe1l <sgadgsfad@gmail.com>
        Date:   Sun Aug 31 19:41:49 2025 +0300
    
            home-main-cart relation to db
    
        commit bcc918c7815b9c1734ba12253e25e5007e456e05
        Author: iamfromhe1l <sgadgsfad@gmail.com>
        Date:   Sat Aug 30 14:51:30 2025 +0300
    
            base collections + home-page carousel
    
        commit e92341f4db7dc91c5c7fd4dd09b4d47a9d42feef
        Author: iamfromhe1l <sgadgsfad@gmail.com>
        Date:   Thu Aug 28 11:50:21 2025 -0700
    
            yarn lock
    
        commit 29fd8fb1d0f283e5bdeeaffadca1c38fcbc80818
        Merge: aa58f8c 4eecb4d
        Author: iamfromhe1l <sgadgsfad@gmail.com>
        Date:   Thu Aug 28 11:49:52 2025 -0700
    
            Merge branch 'dev' into home-page
    
        commit aa58f8cf2cb6ba62710391251292467f1146a45c
        Author: iamfromhe1l <sgadgsfad@gmail.com>
        Date:   Mon Aug 25 13:08:01 2025 -0700
    
            footer
    
        commit c14d21badc869db32190a6a89b2dbb014b5f2587
        Author: iamfromhe1l <sgadgsfad@gmail.com>
        Date:   Mon Aug 25 12:12:37 2025 -0700
    
            home-info-card
    
        commit e7351ca1186a696de5b3027827d409cf6278e02e
        Author: iamfromhe1l <sgadgsfad@gmail.com>
        Date:   Sun Aug 24 02:24:37 2025 -0700
    
            home page main card + theme

--- src/components/home-main-card/HomeMainCard.tsx
+++ src/components/home-main-card/HomeMainCard.tsx
@@ -34,37 +34,54 @@ export const HomeMainCard: React.FC<IHomeMainCardProps> = ({
             <Card className="bg-muted relative h-[960px] overflow-hidden rounded-4xl py-2 shadow-none sm:py-4">
               <Image
                 className="absolute z-10 h-full w-full object-cover"
-                src={(mainPageProduct['background-image'] as Media).url ?? 'home-main-bg.png'}
-                alt={(mainPageProduct['background-image'] as Media).alt ?? 'image-alt'}
+                src={
+                  (mainPageProduct['background-image'] as Media).url ??
+                  'home-main-bg.png'
+                }
+                alt={
+                  (mainPageProduct['background-image'] as Media).alt ??
+                  'image-alt'
+                }
                 quality={100}
                 fill
               />
               <CardContent className="z-20 flex h-full flex-col justify-center px-2 sm:px-4">
                 <div className="flex flex-1 flex-col items-center justify-center gap-2 text-center text-white/80">
                   <p className="font-mono text-4xl">NobleWave</p>
-                  <p className="text-5xl font-black md:text-8xl">{(mainPageProduct.product as Product).title}</p>
-                  <p className="text-4xl font-black md:text-7xl">{(mainPageProduct.product as Product).subtitle}</p>
+                  <p className="text-5xl font-black md:text-8xl">
+                    {(mainPageProduct.product as Product).title}
+                  </p>
+                  <p className="text-4xl font-black md:text-7xl">
+                    {(mainPageProduct.product as Product).subtitle}
+                  </p>
                 </div>
                 <div className="flex flex-col gap-2">
                   <Card className="bg-background/10 rounded-3xl py-3 backdrop-blur-md sm:w-1/2 xl:w-1/3">
                     <CardContent className="text-white">
                       <article className="prose prose-invert">
-                        <PayloadLexicalReactRenderer content={(mainPageProduct.product as Product).description as PayloadLexicalReactRendererContent} />
+                        <PayloadLexicalReactRenderer
+                          content={
+                            (mainPageProduct.product as Product)
+                              .description as PayloadLexicalReactRendererContent
+                          }
+                        />
                       </article>
                     </CardContent>
                   </Card>
                   <Card className="bg-background/10 rounded-3xl py-3 backdrop-blur-md">
                     <CardContent className="flex h-full flex-col gap-4 px-3 sm:flex-row sm:items-center">
                       <div className="grid flex-1 grid-cols-2 flex-wrap gap-1 sm:flex">
-                        {(mainPageProduct.product as Product).badges?.map((badge, index) => (
-                          <Badge
-                            key={index}
-                            variant="outline"
-                            className="w-full py-2 whitespace-normal sm:w-auto"
-                          >
-                            {badge.title}
-                          </Badge>
-                        ))}
+                        {(mainPageProduct.product as Product).badges?.map(
+                          (badge, index) => (
+                            <Badge
+                              key={index}
+                              variant="outline"
+                              className="w-full py-2 whitespace-normal sm:w-auto"
+                            >
+                              {badge.title}
+                            </Badge>
+                          ),
+                        )}
                       </div>
                       <Button size="lg">
                         Подробнее <ChevronRight />
@@ -79,6 +96,6 @@ export const HomeMainCard: React.FC<IHomeMainCardProps> = ({
       </CarouselContent>
       <CarouselNext variant="ghost" />
       <CarouselPrevious variant="ghost" />
-    </Carousel >
+    </Carousel>
   );
 };
